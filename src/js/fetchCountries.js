0ec298de7a3bc09f49d9a1065abbefee4ff42e4b
ElenaPrepodobnaja <73935522+ElenaPrepodobnaja@users.noreply.github.com>
2021-05-28 20:26:43 +0300

    12-4

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,6 @@
 
  function fetchCountry(name) {
-      return fetch(`https:restcountries.eu/rest/v2/name/${name}`)
+      return fetch(`https://restcountries.eu/rest/v2/name/${name}`)
          .then(response => 
               response.json()
          );
